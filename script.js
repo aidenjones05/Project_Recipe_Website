a2200d86fe1c24a400a9d8ccef1014ee7587a622
aidenjones05 <aiden.jones05@gmail.com>
2025-10-08 22:28:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// This runs after the page loads
+const homeButton = document.getElementById('homeButton');
+
+homeButton.addEventListener('click', () => {
+  // Navigate to home page
+  window.location.href = 'index.html'; // or '/' for your live site
+});

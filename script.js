b640ff4a4f917b759008b59dbad313d92d29aa0d
aidenjones05 <aiden.jones05@gmail.com>
2025-10-08 23:22:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // script.js
 console.log('JS connected!');
-alert('Hello! JavaScript is working!');
-// This runs after the page loads
+
 const homeButton = document.getElementById('homeButton');
 
 homeButton.addEventListener('click', () => {
-  // Navigate to home page
-  window.location.href = '/'; // or '/' for your live site
+  window.location.href = '/';
 });
+
 
 
 
